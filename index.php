4f6c82b67fa3e6ac6cdcf680d48063188c792505
xpyctum <basalygaoleg@yandex.ru>
2017-05-08 00:18:37 +0500

    Fixed duplicated to answered messages in ONE session

--- index.php
+++ index.php
@@ -4,12 +4,17 @@ const TOKEN = "";
 const TIMEOUT_REFRESH = 0.5;
 const API_METHOD_URL = "https://api.vk.com/method/";
 
+/** @var array $messages_layers */
 $messages_layers = [
     'вопрос' => 'ответ',
     'привет' => 'Это бот',
     'пока' => 'саси',
     '/help' => 'помощь'
 ];
+
+/** @var array $ansered_messages */
+$answered_messages = [];
+
 while(true) {
     $j = json_decode(file_get_contents(API_METHOD_URL."messages.get?count=1&out=0&time_offset=30&v=5.62&access_token=" . TOKEN), true);
     //print_r($j);
@@ -22,19 +27,21 @@ while(true) {
                 $read_state = $item['read_state'];
                 $user_id = $item['user_id'];
                 $mid = $item['id'];
-                foreach ($messages_layers as $key => $answer) {
-                    if ($text == $key) {
-                        $random_value = microtime(true);
-                        $request_params = [
-                            'user_id' => $user_id,
-                            'random_id' => $random_value,
-                            'message' => $answer,
-                            'v' => '5.62',
-                            'access_token' => TOKEN
-                        ];
-                        if ($mid !== $random_value) {
-                            $url = API_METHOD_URL."messages.send?" . http_build_query($request_params);
+                if(!isset($answered_messages[$mid])) {
+                    foreach ($messages_layers as $key => $answer) {
+                        if ($text == $key) {
+                            $random_value = microtime(true);
+                            $request_params = [
+                                'user_id' => $user_id,
+                                'random_id' => $random_value,
+                                'message' => $answer,
+                                'v' => '5.62',
+                                'access_token' => TOKEN
+                            ];
+                            $url = API_METHOD_URL . "messages.send?" . http_build_query($request_params);
                             file_get_contents($url);
+                            echo "[ ".date('H:i:s')." ] Answered to ".$mid." message.";
+                            $answered_messages[$mid] = true;
                         }
                     }
                 }
