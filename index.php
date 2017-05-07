724ad0518a4d24d635ff813a7bf34dec5930e031
DonkihotD <Kb08ek@mail.ru>
2017-05-07 23:44:38 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,13 @@ $mes = [
     '/help' => 'помощь'
 ];
 
-for($i = 0; $i < 30; $i++){
+    $j = json_decode(file_get_contents("https://api.vk.com/method/messages.get?count=1&out=0&time_offset=30&v=5.62&access_token=".$token),true);
+    //print_r($j);
+    $text = $j['response']['items'][0]['body'];
+    $read = $j['response']['items'][0]['read_state'];
+    $id = $j['response']['items'][0]['user_id'];
+    $mid = $j['response']['items'][0]['id'];
     foreach($mes as $key => $value){
-        $j = json_decode(file_get_contents("https://api.vk.com/method/messages.get?count=1&out=0&time_offset=30&v=5.62&access_token=".$token),true);
-        //print_r($j);
-        $text = $j['response']['items'][0]['body'];
-    	$read = $j['response']['items'][0]['read_state'];
-    	$id = $j['response']['items'][0]['user_id'];
-        $mid = $j['response']['items'][0]['id'];
         if($text == $key){
             $rand = mt_rand(20, 99999999);
             $request_params = [
@@ -31,5 +30,4 @@ for($i = 0; $i < 30; $i++){
             }
         }
     }
-}
 ?>
